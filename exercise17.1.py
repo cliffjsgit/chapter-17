09c055443b483d332b52417e4f2816c5427f82ef
Philip Ulrich <philip@ulrich.life>
2017-10-17 21:56:42 +0000

    intitial commit

---
+++ exercise17.1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+###############################################################################
+#
+print("\nExercise 17.1\n")
+#
+# Question 1
+# 1. Download the code from this chapter from :
+#    http://thinkpython2.com/code/Time2.py. 
+# Change the attributes of Time to be a single integer representing seconds 
+# since midnight. Then modify the methods (and the function int_to_time) to work
+# with the new implementation. You should not have to modify the test code in 
+# main. When you are done, the output should be the same as before.
+#
---
+++ exercise17.2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+###############################################################################
+#
+print("\nExercise 17.2\n")
+#
+# Question 1
+# 1. This exercise is a cautionary tale about one of the most common, and 
+# difficult to find, errors in Python. Write a definition for a class named 
+# Kangaroo with the following methods:
+#
+#     1. An __init__ method that initializes an attribute named pouch_contents
+#        to an empty list.
+#     2. A method named put_in_pouch that takes an object of any type and adds
+#        it to pouch_contents.
+#     3. A __str__ method that returns a string representation of the Kangaroo 
+#        object and the contents of the pouch.
+#
+# Test your code by creating two Kangaroo objects, assigning them to variables 
+# named kanga and roo, and then adding roo to the contents of kanga's pouch.
+#
+# Download http://thinkpython2.com/code/BadKangaroo.py. It contains a solution
+# to the previous problem with one big, nasty bug. Find and fix the bug.
+# 
+# If you get stuck, you can download http://thinkpython2.com/code/GoodKangaroo.py
+# which explains the problem and demonstrates a solution.
+# 
